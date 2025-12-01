aae6a9dacd80ba29882c72a59e3977e879794572
OttNorml <2350859+ottnorml@users.noreply.github.com>
2025-12-01 05:34:59 +0100

    chore: update flake inputs to 25.11 release versions for NixOS, Home Manager, and nix-darwin

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   description = "Starter Configuration with secrets for MacOS and NixOS";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
     agenix.url = "github:ryantm/agenix";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-homebrew = {
--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -58,7 +58,7 @@ in
           { "emacs-launcher.command".source = myEmacsLauncher; }
         ];
 
-        stateVersion = "25.05";
+        stateVersion = "25.11";
       };
 
       # Extend the zsh configuration
