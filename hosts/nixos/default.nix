eb35de59347646ae303e5f860eb68bd48a0c33dc
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-03-03 19:41:42 +0100

    chore(nixos): update timezone to Europe/Berlin

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -29,7 +29,7 @@ in
   };
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "Europe/Berlin";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
