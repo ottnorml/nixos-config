f3f329ce877191caefc26e907f54c58e8d87894b
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-04-09 07:59:42 +0200

    chore(darwin): set `CLOUDSDK_PYTHON` environment variable to use Python 3.13

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -24,6 +24,7 @@ in
       HOMEBREW_NO_ANALYTICS = "1";
       HOMEBREW_NO_INSECURE_REDIRECT = "1";
       HOMEBREW_NO_ENV_HINTS = "0";
+      CLOUDSDK_PYTHON = "${pkgs.python313}/bin/python3";
     };
 
     # This is included so that the homebrew packages are available in the PATH.
