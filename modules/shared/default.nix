7f4d18ead8f0a549a878a85648d29d5e4c935d2f
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-04-20 06:15:44 +0200

    chore: extend SSH client and server configuration with restricted algorithms in shared module

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -4,12 +4,19 @@ let
   emacsOverlaySha256 = "11p1c1l04zrn8dd5w8zyzlv172z05dwi9avbckav4d5fk043m754";
 in
 {
-  # Shared ssh configration
+  # Shared ssh client configration
   programs.ssh.extraConfig = lib.mkAfter ''
     Host *
-      HostKeyAlgorithms -*ecdsa-sha2-nistp*
+      HostKeyAlgorithms -ecdsa-sha2-nistp*,sk-ecdsa-sha2-nistp*
       KexAlgorithms -ecdh-sha2-nistp*
       MACs -hmac-sha1*
+  '';
+
+  # Shared ssh server configration
+  services.openssh.extraConfig = lib.mkAfter ''
+    HostKeyAlgorithms -ecdsa-sha2-nistp*,sk-ecdsa-sha2-nistp*
+    KexAlgorithms -ecdh-sha2-nistp*
+    MACs -hmac-sha1*
   '';
 
   nixpkgs = {
