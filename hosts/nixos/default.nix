e4fec61262a68243659faf92b70834c3b549bb7f
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-04-28 23:08:03 +0200

    chore(nix): add `numtide` cache to substituters and trusted keys

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -62,10 +62,12 @@ in
 
       extra-substituters = [
         "https://nix-community.cachix.org"
+        "https://cache.numtide.com"
       ];
 
       extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
 
