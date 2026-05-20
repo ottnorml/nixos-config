9ccbe7288fa98f5e3d54ab9b4682b5accfcf6bf9
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-05-20 02:50:42 +0200

    chore: bump Homebrew to version 5.1.12 in flake configuration

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     # Override Homebrew version to prevent nix-homebrew from using its pinned older version
     # (reference: https://github.com/zhaofengli/nix-homebrew/blob/a7760a3a83f7609f742861afb5732210fdc437ed/flake.nix)
     homebrew-brew = {
-      url = "github:Homebrew/brew/5.1.11";
+      url = "github:Homebrew/brew/5.1.12";
       flake = false;
     };
 
