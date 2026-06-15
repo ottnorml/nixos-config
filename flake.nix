b60b6d6251a39c4a39c86699d2feafc174df36da
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-06-15 19:41:08 +0200

    chore(flake.nix): bump Homebrew version to 6.0.2

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
     # Override Homebrew version to prevent nix-homebrew from using its pinned older version
     # (reference: https://github.com/zhaofengli/nix-homebrew/blob/a7760a3a83f7609f742861afb5732210fdc437ed/flake.nix)
     homebrew-brew = {
-      url = "github:Homebrew/brew/6.0.1";
+      url = "github:Homebrew/brew/6.0.2";
       flake = false;
     };
 
