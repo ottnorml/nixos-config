0e9c6340152fe9e73904d707be4b2f0e6bab8def
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-06-19 14:10:35 +0200

    feat(nix): add `cache.numtide.com` to trusted public keys for NixOS and Darwin configurations

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -62,6 +62,7 @@ in
 
       extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.numtide.com-1:GF3TabtFocLtonIGfz3PD61AgIO8GmjCYhEAmYy4VPY="
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
