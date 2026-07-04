f7a86b27b6429fd3b98096bca3b40c291eef8cd1
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-07-04 13:36:13 +0200

    fix(darwin): adjust GC schedule to run on Sundays at 3:15 AM

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -70,9 +70,9 @@ in
     gc = {
       automatic = true;
       interval = {
-        Weekday = 0;
-        Hour = 2;
-        Minute = 0;
+        Hour = 3;
+        Minute = 15;
+        Weekday = 7; # Sunday
       };
       options = "--delete-older-than 30d";
     };
