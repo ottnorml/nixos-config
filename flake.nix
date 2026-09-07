510c6b21951733603a2c9b1fba659165a390b04d
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-09-07 18:51:26 +0200

    fix(flake): bump Homebrew version to 6.0.22 and update `flake.lock` file
    • Updated input 'homebrew-brew':
        'github:Homebrew/brew/560147012b9678b42ef5e83b690f0895552d1366' (2026-09-01)
      → 'github:Homebrew/brew/08e85c4e42f5d8f1ea17c36cb59cf61c2ccb26c3' (2026-09-05)

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
     # Explicit Homebrew version override for Casks requiring newer features.
     # Reference: https://github.com/zhaofengli/nix-homebrew/blob/a7760a3a83f7609f742861afb5732210fdc437ed/flake.nix
     homebrew-brew = {
-      url = "github:Homebrew/brew/6.0.21";
+      url = "github:Homebrew/brew/6.0.22";
       flake = false;
     };
 
